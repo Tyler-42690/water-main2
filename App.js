a448bb06012aa9cbe868ee526c1dac40559f6af4
Palacios1337 <98562237+Palacios1337@users.noreply.github.com>
2023-02-28 12:35:45 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Nav from "./src/nav"
+
+export default function App() {
+  return (
+    <View style={{flex:1}}>
+      <Nav/>
+    </View>
+  )
+}
